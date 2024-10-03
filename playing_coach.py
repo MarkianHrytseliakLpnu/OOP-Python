8913fba8bc17ffd34b13703e0a3d07d17a9d2019
MarkianHrytseliakLpnu <146416671+MarkianHrytseliakLpnu@users.noreply.github.com>
2024-10-03 11:16:50 +0300

    Update playing_coach.py

--- playing_coach.py
+++ playing_coach.py
@@ -5,14 +5,14 @@ class PlayingCoach(Player, Coach):
 
     def __init__(self, name, age, position, salary):
         super().__init__(name, age, position, salary)
-        self.is_hired=False
+        self.is_hired = False
 
     def set_info(self, name, age, position, salary):
         super().set_info(name, age, position, salary)
 
     def hire_to_club(self):
         print('Playing Coach hired to club')
-        self.is_hired=True
+        self.is_hired = True
 
     @staticmethod
     def print_info(name, age, position, salary):
