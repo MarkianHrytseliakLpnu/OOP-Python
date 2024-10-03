171c4d0b2b32aefd6373b1cc55530e8211edf36f
MarkianHrytseliakLpnu <146416671+MarkianHrytseliakLpnu@users.noreply.github.com>
2024-10-03 11:10:34 +0300

    Update coach.py

--- coach.py
+++ coach.py
@@ -1,8 +1,10 @@
 class Coach:
+
     def __init__(self, name, age, salary):
         self.name = name
         self.age = age
         self.salary = salary
+        self.is_hired = False
 
     def set_info(self, name, age, salary):
         self.name = name
@@ -11,6 +13,7 @@ class Coach:
 
     def hire_to_club(self):
         print('Coach hired to club')
+        self.is_hired = True
 
     @staticmethod
     def print_info(name, age, salary):
