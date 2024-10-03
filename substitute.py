4a42127f178f63ef78d9cb8a8f5f7772c9ce2853
MarkianHrytseliakLpnu <146416671+MarkianHrytseliakLpnu@users.noreply.github.com>
2024-10-03 11:15:08 +0300

    Update substitute.py

--- substitute.py
+++ substitute.py
@@ -5,12 +5,14 @@ class Substitute(Player):
 
     def __init__(self, name, age, position, salary):
         super().__init__(name, age, position, salary)
+        self.is_added = False
 
     def set_info(self, name, age, position, salary):
         super().set_info(name, age, position, salary)
 
     def add_to_club(self):
         print('Substitute added to club')
+        self.is_added = True
 
     @staticmethod
     def print_info(name, age, position, salary):
