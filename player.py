7ba6ded2017569c9322857a1ed42234a450e4b6c
MarkianHrytseliakLpnu <146416671+MarkianHrytseliakLpnu@users.noreply.github.com>
2024-10-03 11:11:51 +0300

    Update player.py

--- player.py
+++ player.py
@@ -1,9 +1,11 @@
 class Player:
+
     def __init__(self, name, age, position, salary):
         self.name = name
         self.age = age
         self.position = position
         self.salary = salary
+        self.is_added = False
 
     def set_info(self, name, age, position, salary):
         self.name = name
@@ -13,8 +15,8 @@ class Player:
 
     def add_to_club(self):
         print('Player added to club')
+        self.is_added = True
 
     @staticmethod
     def print_info(name, age, position, salary):
-        # Використовуємо правильне форматування
         print(f'Name: {name}\nAge: {age}\nPosition: {position}\nSalary: {salary}')
