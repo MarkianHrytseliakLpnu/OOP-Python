de9a5acace235ba3e1c680bd47238d167bfd243d
MarkianHrytseliakLpnu <146416671+MarkianHrytseliakLpnu@users.noreply.github.com>
2024-10-03 11:11:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from coach import Coach
 from substitute import Substitute
 from playing_coach import PlayingCoach
 
-# Створення об'єктів і виклик методів
 coach1 = Coach('Andrew Lando', 50, 10000)
 coach2 = PlayingCoach('John Conrad', 30, 'GK', 15000)
 
-# Порівняння зарплат і вибір тренера
+
 if coach1.salary < coach2.salary:
     coach1.print_info(coach1.name, coach1.age, coach1.salary)
     coach1.hire_to_club()
@@ -25,7 +24,6 @@ sub2 = Substitute('Max Freeman', 23, 'LW', 12000)
 player2.set_info('Stan Cook', 29, 'CM', 22000)
 sub1.set_info('Jack Tingly', 36, 'CB', 11500)
 
-# Додавання гравців у клуб
 Player.print_info(player1.name, player1.age, player1.position, player1.salary)
 player1.add_to_club()
 print()
